8478d60b75d5f8d37f7463c211305ff2c1a390c5
AlexBulavkin <alexbulavkin@gmail.com>
2022-06-14 01:46:44 +0700

    first commit

---
+++ api_yamdb/reviews/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+
+class Category(models.Model):
+    pass
+
+
+class Genre(models.Model):
+    pass
+
+
+class Title(models.Model):
+    pass
+
+
+class GenreTitle(models.Model):
+    pass
+
+
+class Review(models.Model):
+    pass
+
+
+class Comment(models.Model):
+    pass
