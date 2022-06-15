e44b9c1f914b55c57eb7358c527d59f1a5e5eb24
AlexBulavkin <alexbulavkin@gmail.com>
2022-06-15 21:59:54 +0700

    users

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -117,7 +117,20 @@ AUTH_USER_MODEL = 'users.User'
 
 REST_FRAMEWORK = {
 
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ],
+
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ),
+
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
 }
+
+
+# Emails
+
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -9,8 +9,15 @@ CHOICES_ROLES = (
 
 
 class User(AbstractUser):
-    role = models.CharField(max_length=16, choices=CHOICES_ROLES)
+    username = models.CharField(blank=False, unique=True, max_length=150)
+    email = models.CharField(blank=False, unique=True, max_length=254)
+    first_name = models.CharField(blank=True, max_length=150)
+    last_name = models.CharField(blank=True, max_length=150)
     bio = models.TextField(
         'Биография',
         blank=True,
     )
+    role = models.CharField(
+        default='user',
+        max_length=16,
+        choices=CHOICES_ROLES)
