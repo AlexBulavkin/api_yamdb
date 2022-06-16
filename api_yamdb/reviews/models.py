6e20b1028ebbfa63b0cf3225af257cbc4df4a0d9
VankoID <dudarev.id@gmail.com>
2022-06-16 16:26:23 +0400

    Titles

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,18 +1,32 @@
+from unicodedata import category
 from django.db import models
 
 
 class Category(models.Model):
-    name = models.TextField(max_length=50, verbose_name='Категория')
+    name = models.CharField(max_length=50, verbose_name='Категория')
     slug = models.SlugField(unique=True, max_length=50)
 
 
 class Genre(models.Model):
-    name = models.TextField(max_length=50, verbose_name='Жанр')
+    name = models.CharField(max_length=50, verbose_name='Жанр')
     slug = models.SlugField(unique=True, max_length=50)
 
 
 class Title(models.Model):
-    pass
+    name = models.CharField(max_length=50, unique=True)
+    year = models.DateTimeField(verbose_name='Дата выхода фильма',)
+    description = models.TextField(
+        max_length=300,
+        verbose_name='Описание фильма'
+    )
+    genre = models.ForeignKey(
+        Genre,
+        on_delete=models.SET_NULL
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL
+    )
 
 
 class GenreTitle(models.Model):
