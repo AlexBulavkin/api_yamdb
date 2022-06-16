b744f1349a30885c26017e50921b72144a2a370c
VankoID <dudarev.id@gmail.com>
2022-06-16 16:00:51 +0400

    Titles

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -2,12 +2,13 @@ from django.db import models
 
 
 class Category(models.Model):
-    slug = models.SlugField(unique=True, max_length=50,)
-    pass
+    name = models.TextField(max_length=50, verbose_name='Категория')
+    slug = models.SlugField(unique=True, max_length=50)
 
 
 class Genre(models.Model):
-    pass
+    name = models.TextField(max_length=50, verbose_name='Жанр')
+    slug = models.SlugField(unique=True, max_length=50)
 
 
 class Title(models.Model):
