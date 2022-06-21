4f517e2b54932a587127bed9a212a54f516d7bb3
Олег Пронькин <o_pronkin@mail.ru>
2022-06-21 09:02:21 +0300

    views and 1_permission

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -24,3 +24,11 @@ class ReadOnly(permissions.BasePermission):
 
     def has_permission(self, request, view):
         return request.method in permissions.SAFE_METHODS
+
+
+class IsAdminModeratorOwnerOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return (request.method in permissions.SAFE_METHODS
+                or request.user.is_admin
+                or request.user.is_moderator
+                or obj.author == request.user)
